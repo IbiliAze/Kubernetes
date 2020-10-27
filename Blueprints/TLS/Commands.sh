f827424c26cd56dff805b364b67d42b0703d3600
Ibi Hasanli <ibili73@gmail.com>
2020-10-27 02:33:48 -0700

    aliased commit on ubuntu

---
+++ Blueprints/TLS/Commands.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+[ Custom Certificates ]
+
+#CSR
+cat <<EOF | cfssl genkey - | cfssljson -bare server
+{
+    "hosts": [
+        "my-svc.my-namespace.svc.cluster.local",
+        "my-pod.my-namespace.pod.cluster.local",
+        "172.168.0.24",
+        "10.0.34.2"
+    ],
+    "CN": "my-pod.my-namespace.pod.cluster.local",
+    "key": {
+        "algo": "ecdsa",
+        "size": 256
+    }
+}
+EOF 
+
+
+
+[ Certificates ]
+
+kubectl get csr
+
+kubectl get csr pod-csr.web -o yaml
+
+kubectl get csr pod-csr.web -o jsonpath='{.status.certificate}' | base64 --decode #our certificate
+
+kubectl describe csr pod-csr.web
+
+kubectl certificate approve pod-csr.web
+
+kubectl exec testapp-888d4f679-wbz6x -- ls /var/run/secrets/kubernetes.io/serviceaccount
+
+
+
+
+
