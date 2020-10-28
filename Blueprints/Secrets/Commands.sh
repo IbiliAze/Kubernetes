f55f293edebff7c8aa2972cb01dac4722d5dcfbe
Ibi Hasanli <ibili73@gmail.com>
2020-10-28 03:59:31 -0700

    aliased commit on ubuntu

--- Blueprints/Secrets/Commands.sh
+++ Blueprints/Secrets/Commands.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
+[ Create Secrets ]
+
+kubectl create secret generic example-https --from-file=myKey.key --from-file=myKey.cert --from-file-file
+
+
 
 [ Get Secrets ]
 
 kubectl get secret testacc-token-694gs
+
+kubectl get secrets example-https -o yaml
 
 
 [ Delete Secrets ]
