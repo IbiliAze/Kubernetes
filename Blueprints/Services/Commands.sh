abff52a1a2fe91ddb8e14d143448110703828f8a
Ibi Hasanli <ibili73@gmail.com>
2020-11-19 06:41:31 -0800

    aliased commit on ubuntu

--- Blueprints/Services/Commands.sh
+++ Blueprints/Services/Commands.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 
-[ Get Services ]
+[ Services ]
 
 kubectl get svc
 
-
-
-[ Delete Services ]
-
 kubectl delete svc --all
 
-
-
-[ Annotate Services ]
-
 kubectl annotate service kubernetes externalTrafficPolicy=Local #loadbalancer send traffic to only 1 pod of the node
+
+
+
+[ Expose ]
+
+kubectl expose deployment MyDeployment --port 80 --type NodePort
+
+kubectl expose deployment MyDeployment --port 80 --type NodePort --target-port 8080
+
+kubectl expose deployment MyDeployment --port 80 --type LoadBalancer --targetPort 8080
+
+
+
+[ Endpoints ]
+
+kubectl get endpoints #cache of IPs attached to services
 
 
--- Commands/Kubernetes.sh
+++ Commands/Kubernetes.sh
@@ -79,9 +79,21 @@ kuebadm token list #list current tokens before adding to an existing cluster
 docker inspect --format '{{ .State.Pid }}' ContainerId #get container process ID
 nsenter -n -t MyContainerPid ip addr #-n=network namespace -> empty=pid NS, -t=target, run a command within the processes' network namespace
 
-kubectl get deployment -n kube-system #other services, including DNS
+sudo iptables-save | grep KUBE #iptables rules
 
+
+{DNS}
+kubectl get pods -n kube-system
+kubectl get deployment -n kube-system #other services, including DNS
 kubectl get services -n kube-system #get DNS server IP address
+kubectl exec -it node-54fdfb47f8-6w7vk -- cat /etc/resolv.conf #should return the same DNS server IP as the kube-dns service above
+
+kubectl exec -it node-54fdfb47f8-6w7vk -- apt-get install dnsutils -y
+kubectl exec -it node-54fdfb47f8-6w7vk -- nslookup kubernetes #get the domain name of the kubernetes ClusterIP service
+kubectl exec -it node-54fdfb47f8-6w7vk -- nslookup 10-4-2-5.default.pod.cluster.local #nslookup pods
+kubectl exec -it node-54fdfb47f8-6w7vk -- nslookup metrics-server.default.svc.cluster.local #nslookup services
+
+kubectl logs coredns-4234b2342424-2vfv -n kube-system
 
 
 
