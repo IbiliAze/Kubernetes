be4f7e33e62eb12d9f643d4ef098f7bb49184a5d
Ibi Hasanli <ibili73@gmail.com>
2020-12-22 01:10:46 -0800

    aliased commit on ubuntu

--- Projects/Istio/Installation.sh
+++ Projects/Istio/Installation.sh
@@ -3,16 +3,16 @@
 
 
 echo step 1
-wget https://github.com/istio/istio/releases/download/1.0.6/istio-1.0.6-linux.tar.gz
+wget https://github.com/istio/istio/releases/download/1.8.1/istio-1.8.1-linux.tar.gz
 
 echo step 2
-tar -xvf istio-1.0.6-linux.tar.gz
+tar -xvf istio-1.8.1-linux.tar.gz
 
 echo step 3
-mv istio-1.0.6/bin/istioctl /usr/bin
+mv istio-1.8.1/bin/istioctl /usr/bin
 
 echo step 4
-kubectl apply -f istio-1.0.6/install/kubernetes/istio-demo.yaml
+kubectl apply -f istio-1.8.1/install/kubernetes/istio-demo.yaml
 
 echo step 5
 kubectl get pods -n istio-system
